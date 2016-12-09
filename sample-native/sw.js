e1503866ca74d9238cfa01036471022fd9e81d04
Anatoliy Ostapenko <ostapencko.anatoliy@yandex.ru>
2016-12-09 13:38:42 +0300

    fix error

--- sample-native/sw.js
+++ sample-native/sw.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var CACHE_NAME = 'v1',
-	MAX_AGE = 3600000,
 	cacheUrls = [
 		'index.html',
 	];
@@ -21,39 +20,27 @@ self.addEventListener('activate', function (event) {
 
 self.addEventListener('fetch', function (event) {
 	event.respondWith((function () {
-		return caches.match(event.request)
+		return caches
+			.match(event.request)
 			.then(function (cachedResponse) {
-				var doUpdate = false;
-				var lastModified, fetchRequest;
-
 				if (cachedResponse) {
-					lastModified = new Date(cachedResponse.headers.get('last-modified'));
-					if (lastModified && (Date.now() - lastModified.getTime()) > MAX_AGE) {
-						doUpdate = true;
-					}
-				} else {
-					doUpdate = true;
-				}
-
-				if (doUpdate) {
-					fetchRequest = event.request.clone();
-					return fetch(fetchRequest)
-						.then(function (response) {
-							if (!response || response.status !== 200) {
-								return cachedResponse;
-							}
-							var clonedResponse = response.clone();
-							caches.open(CACHE_NAME).then(function (cache) {
-								cache.put(event.request, response.clone());
-							});
-							return clonedResponse;
-						})
-						.catch(function () {
-							return cachedResponse;
-						});
-				} else {
 					return cachedResponse;
 				}
+
+				return fetch(event.request)
+					.then(function (response) {
+						if (!response || response.status !== 200) {
+							return cachedResponse;
+						}
+						var clonedResponse = response.clone();
+						caches.open(CACHE_NAME).then(function (cache) {
+							cache.put(event.request, response.clone());
+						});
+						return clonedResponse;
+					})
+					.catch(function () {
+						return cachedResponse;
+					});
 			})
 	})());
 });
