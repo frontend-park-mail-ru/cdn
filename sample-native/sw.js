26c97a4e0b942b6df70e8ddb77125e901bc2dc32
Anatoliy Ostapenko <ostapencko.anatoliy@yandex.ru>
2016-12-09 14:04:18 +0300

    fix error 2

--- sample-native/sw.js
+++ sample-native/sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var CACHE_NAME = 'v2',
+var CACHE_NAME = 'v3',
 	cacheUrls = [
 		'https://code.jquery.com/jquery-3.1.1.min.js',
 		'https://cdn.jsdelivr.net/semantic-ui/2.2.6/semantic.min.css',
